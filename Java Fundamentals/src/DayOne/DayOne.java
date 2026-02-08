37ede8a692228e2c2a24bd71ea8400739561d9f5
Shubham Kondekar <kondekarshubham123@gmail.com>
2026-02-08 13:18:03 +0530

    Day one progress

---
+++ Java Fundamentals/src/DayOne/DayOne.java
@@ -0,0 +1,12 @@
+package DayOne;
+
+public class DayOne {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Day 1");
+
+        // All the things you learnt in day 1;
+
+    }
+}
