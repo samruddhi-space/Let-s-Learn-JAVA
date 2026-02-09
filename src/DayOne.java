53ce0808adc48737b1e09f45cf7e63e9709d20b3
samruddhi-space <samruddhijunavane@gmail.com>
2026-02-09 19:26:57 +0530

    Addition

---
+++ src/DayOne.java
@@ -0,0 +1,11 @@
+class DayOne{
+    public static void main(String [] a)
+    {
+        int num1 = 3;
+        int num2 = 9;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1+num2);
+}
+}
