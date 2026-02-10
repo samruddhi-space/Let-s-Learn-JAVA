46270076170e324c86ed7b7b1bd16d59e408553a
samruddhi-space <samruddhijunavane@gmail.com>
2026-02-10 21:02:22 +0530

    DayOne Progress Submission

--- Java Fundamentals/src/DayOne/DayOne.java
+++ Java Fundamentals/src/DayOne/DayOne.java
@@ -7,6 +7,12 @@ public class DayOne {
         System.out.println("Welcome to Day 1");
 
         // All the things you learnt in day 1;
+        int num1 = 3;
+        int num2 = 9;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1+num2);
 
     }
 }
